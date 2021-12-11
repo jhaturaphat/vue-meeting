0e937c04f546fe37bed6d12829f2928f8830b498
jhaturaphat <umt.eng52@gmail.com>
2021-12-12 06:14:02 +0700

    XXX

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const server = express();
 
+server.get('*',(req, res) => {
+    res.send('<h1>Server started</h1>')
+})
 
 server.listen(5000, ()=>{console.log("Server is start On PORT 5000");})
