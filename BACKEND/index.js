b7fbeeb779b1703e06d1a474d561b332c2addf73
jhaturaphat <umt.eng52@gmail.com>
2021-12-12 07:20:50 +0700

    setup backend project

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const server = express();
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session')
 
-server.get('*',(req, res) => {
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { /*secure: true*/ }
+  }))
+
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
+app.get('*',(req, res) => {
     res.send('<h1>Server started</h1>')
 })
 
-server.listen(5000, ()=>{console.log("Server is start On PORT 5000");})
+app.listen(5000, ()=>{console.log("Server is start On PORT 5000");})
